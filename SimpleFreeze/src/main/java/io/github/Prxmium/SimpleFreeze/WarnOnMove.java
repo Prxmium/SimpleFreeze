8c49988c64166a48278f457d506fcf30a88e7887
TTGTRE <tr3.logan@gmail.com>
2015-01-02 19:23:13 -0500

    Signed-off-by: TTGTRE <tr3.logan@gmail.com>

--- SimpleFreeze/src/main/java/io/github/Prxmium/SimpleFreeze/WarnOnMove.java
+++
@@ -1,5 +0,0 @@
-package io.github.Prxmium.SimpleFreeze;
-
-public class WarnOnMove {
-
-}
---
+++ src/prxmium/simplefreeze/Main.java
@@ -0,0 +1,84 @@
+package prxmium.simplefreeze;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener
+{
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+
+		getLogger().info(getName() + " has been enabled successfully.");
+	}
+
+	public void onDisable()
+	{
+		getLogger().info(getName() + " has been disabled successfully.");
+	}
+
+	private HashMap<Player, Location> frozenPlayers = new HashMap<>();
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if (command.getName().equalsIgnoreCase("freeze"))
+		{
+			if (args.length > 0)
+			{
+				@SuppressWarnings("deprecation")
+				Player player = Bukkit.getPlayer(args[0]);
+
+				frozenPlayers.put(player, player.getLocation());
+
+				player.sendMessage(ChatColor.DARK_AQUA + "You've been frozen!");
+			}
+			else
+			{
+				sender.sendMessage(ChatColor.RED + "Wrong syntax!");
+			}
+
+			return true;
+		}
+		if (command.getName().equalsIgnoreCase("unfreeze"))
+		{
+			if (args.length > 0)
+			{
+				@SuppressWarnings("deprecation")
+				Player player = Bukkit.getPlayer(args[0]);
+
+				frozenPlayers.remove(player);
+
+				player.sendMessage(ChatColor.DARK_AQUA + "You've been unfrozen!");
+			}
+			else
+			{
+				sender.sendMessage(ChatColor.RED + "Wrong syntax!");
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event)
+	{
+		Player player = event.getPlayer();
+
+		if (frozenPlayers.containsKey(player))
+		{
+			player.teleport(frozenPlayers.get(player));
+		}
+	}
+}
