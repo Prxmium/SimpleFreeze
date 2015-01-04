0edae893a9a4caffa41a2d9a86f1b845f3078011
TTGTRE <tr3.logan@gmail.com>
2015-01-04 08:42:40 -0500

    Signed-off-by: TTGTRE <tr3.logan@gmail.com>

--- src/prxmium/simplefreeze/Main.java
+++ src/prxmium/simplefreeze/Main.java
@@ -1,15 +1,6 @@
 package prxmium.simplefreeze;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -26,14 +17,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener
 {
-	private File folderFile = new File("plugins/SimpleFreeze");
-	private File dataFile = new File("plugins/SimpleFreeze/data.txt");
-
-	private final String delimiter = ";";
+	private final DataHandler dataHandler = new DataHandler();
 
 	public void onEnable()
 	{
-		exists();
+		dataHandler.exists();
 
 		getServer().getPluginManager().registerEvents(this, this);
 
@@ -42,7 +30,7 @@ public class Main extends JavaPlugin implements Listener
 
 	public void onDisable()
 	{
-		savePlayerData();
+		dataHandler.savePlayerData(frozenPlayers);
 
 		getLogger().info("Successfully disabled " + getName() + "!");
 	}
@@ -68,7 +56,7 @@ public class Main extends JavaPlugin implements Listener
 				String y = Double.toString(location.getY());
 				String z = Double.toString(location.getZ());
 
-				writeDataSet(playerName + delimiter + x + delimiter + y + delimiter + z + delimiter);
+				dataHandler.writeDataSet(playerName, x, y, z);
 
 				player.sendMessage(ChatColor.DARK_AQUA + "You've been frozen!");
 
@@ -127,7 +115,7 @@ public class Main extends JavaPlugin implements Listener
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event)
 	{
-		loadDataFromFile(event.getPlayer());
+		dataHandler.loadDataFromFile(event.getPlayer(), frozenPlayers);
 	}
 
 	@EventHandler
@@ -139,126 +127,13 @@ public class Main extends JavaPlugin implements Listener
 
 		if (frozenPlayers.containsKey(playerName))
 		{
-			try (PrintWriter writer = new PrintWriter(dataFile))
-			{
-				Location location = frozenPlayers.get(playerName);
+			Location location = frozenPlayers.get(playerName);
 
-				String x = Double.toString(location.getX());
-				String y = Double.toString(location.getY());
-				String z = Double.toString(location.getZ());
+			String x = Double.toString(location.getX());
+			String y = Double.toString(location.getY());
+			String z = Double.toString(location.getZ());
 
-				writer.println(playerName + delimiter + x + delimiter + y + delimiter + z + delimiter);
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
+			dataHandler.writeDataSet(playerName, x, y, z);
 		}
-	}
-
-	public boolean exists()
-	{
-		if (!folderFile.exists() || !dataFile.exists())
-		{
-			folderFile.mkdirs();
-
-			try
-			{
-				dataFile.createNewFile();
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-
-			return false;
-		}
-
-		return true;
-	}
-
-	public void loadDataFromFile(Player player)
-	{
-		try (BufferedReader reader = new BufferedReader(new FileReader(dataFile)))
-		{
-			String line;
-			while ((line = reader.readLine()) != null)
-			{
-				ArrayList<String> data = readDataSet(line);
-
-				if (player.getName().equals(data.get(0)))
-				{
-					double x = Double.parseDouble(data.get(1));
-					double y = Double.parseDouble(data.get(2));
-					double z = Double.parseDouble(data.get(3));
-
-					Location location = new Location(player.getWorld(), x, y, z);
-
-					if (!frozenPlayers.containsKey(player.getName()))
-					{
-						frozenPlayers.put(player.getName(), location);
-					}
-				}
-			}
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void writeDataSet(String dataSet)
-	{
-		try (PrintWriter writer = new PrintWriter(dataFile))
-		{
-			writer.println(dataSet);
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void savePlayerData()
-	{
-		try (PrintWriter writer = new PrintWriter(dataFile))
-		{
-			Set<String> keys = frozenPlayers.keySet();
-
-			for (String key : keys)
-			{
-				@SuppressWarnings("deprecation")
-				Player player = Bukkit.getPlayer(key);
-
-				Location location = player.getLocation();
-
-				String x = Double.toString(location.getX());
-				String y = Double.toString(location.getY());
-				String z = Double.toString(location.getZ());
-
-				writer.println(key + delimiter + x + delimiter + y + delimiter + z + delimiter);
-			}
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public ArrayList<String> readDataSet(String dataSet)
-	{
-		ArrayList<String> data = new ArrayList<>();
-
-		try (Scanner scanner = new Scanner(dataSet))
-		{
-			scanner.useDelimiter(delimiter);
-
-			while (scanner.hasNext())
-			{
-				data.add(scanner.next());
-			}
-		}
-
-		return data;
 	}
 }
